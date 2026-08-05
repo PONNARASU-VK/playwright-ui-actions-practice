bdfa07b10f2a64a79b592578b7fedb99ef3c83b5
PONNARASU-VK <vponnarasu07@gmail.com>
2026-08-05 22:27:39 +0530

    Initial commit

---
+++ tests/actions.spec.ts
@@ -0,0 +1,17 @@
+import {test, expect, Locator} from "@playwright/test";
+
+// Input Box
+test("Verify Input Box", async ({page})=>{
+    await page.goto('file:///C:/GitHub%20Practice/Practice_Site/index.html');
+
+    const name:Locator=page.locator('#nameInput');
+
+    await expect(name).toBeVisible();
+    await expect(name).toBeEnabled();
+    await expect(name).toHaveAttribute('maxlength', '30');
+
+    const testData = 'MSDhoni';
+    await name.fill(testData);
+
+    await expect(name).toHaveValue('MSDhoni');
+});
