accc8d09bef454412f46ef312293feea85825528
PONNARASU-VK <vponnarasu07@gmail.com>
2026-08-08 15:41:26 +0530

    radio button verification

--- tests/actions.spec.ts
+++ tests/actions.spec.ts
@@ -15,3 +15,35 @@ test("Verify Input Box", async ({page})=>{
 
     await expect(name).toHaveValue('MSDhoni');
 });
+
+// Radio Buttons
+test.only("Verify Radio Buttons", async ({page})=>{
+    await page.goto('https://playwright-html-actions.netlify.app/');
+    const Beginner:Locator=page.locator('#expBeginner');
+    const Intermediate:Locator=page.locator('#expIntermediate');
+    const Advanced:Locator=page.locator('#expAdvanced');
+    await expect(Beginner).toBeVisible()
+    await expect(Beginner).toBeEnabled()
+    await expect(Intermediate).toBeVisible()
+    await expect(Intermediate).toBeEnabled()
+    await expect(Advanced).toBeVisible()
+    await expect(Advanced).toBeEnabled()
+    expect(await Beginner.isChecked()).toBe(false);
+    expect(await Intermediate.isChecked()).toBe(false);
+    expect(await Advanced.isChecked()).toBe(false);
+
+    await Beginner.check();
+    await expect(Beginner).toBeChecked()
+    expect(await Intermediate.isChecked()).toBe(false);
+    expect(await Advanced.isChecked()).toBe(false); 
+
+    await Intermediate.check();
+    await expect(Intermediate).toBeChecked()
+    expect(await Beginner.isChecked()).toBe(false);
+    expect(await Advanced.isChecked()).toBe(false);
+
+    await Advanced.check();
+    await expect(Advanced).toBeChecked()
+    expect(await Beginner.isChecked()).toBe(false);
+    expect(await Intermediate.isChecked()).toBe(false);
+});
