6a6825aadc833f392ef3f554e1537a9160bc7806
PONNARASU-VK <vponnarasu07@gmail.com>
2026-08-09 14:27:20 +0530

    checkbox verification tests

--- tests/actions.spec.ts
+++ tests/actions.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect, Locator} from "@playwright/test";
 
 // Input Box
-test("Verify Input Box", async ({page})=>{
+test("Should validate input field accepts text within max length", async ({page})=>{
     await page.goto('https://playwright-html-actions.netlify.app/');
 
     const name:Locator=page.locator('#nameInput');
@@ -17,7 +17,7 @@ test("Verify Input Box", async ({page})=>{
 });
 
 // Radio Buttons
-test("Verify Radio Buttons", async ({page})=>{
+test('Should allow selecting only one radio option at a time', async ({page})=>{
     await page.goto('https://playwright-html-actions.netlify.app/');
     const Beginner:Locator=page.locator('#expBeginner');
     const Intermediate:Locator=page.locator('#expIntermediate');
@@ -47,3 +47,45 @@ test("Verify Radio Buttons", async ({page})=>{
     expect(await Beginner.isChecked()).toBe(false);
     expect(await Intermediate.isChecked()).toBe(false);
 });
+
+// CheckBox
+test('Should handle checkbox selection, deselection, and dynamic state changes correctly', async ({page})=>{
+    await page.goto('https://playwright-html-actions.netlify.app/')
+    const labels:string[]=[' Playwright', ' Selenium', ' Cypress', ' API Testing']
+    
+    // check all the boxes
+    for(let skills of labels){
+        const CheckBox:Locator = page.getByLabel(skills)
+        await expect(CheckBox).toBeVisible()
+        await expect(CheckBox).toBeEnabled()
+        await CheckBox.check()
+        await expect(CheckBox).toBeChecked()
+    }
+
+    // uncheck last two boxes
+    for(let skills of labels.slice(-2)){
+        const CheckBox:Locator = page.getByLabel(skills)
+        await CheckBox.uncheck()
+        await expect(CheckBox).not.toBeChecked()
+    }
+
+    // check the unchecked boxes and uncheck the checked boxes
+    for(let skills of labels){
+        const CheckBox:Locator=page.getByLabel(skills)
+        if(await CheckBox.isChecked()){
+            await CheckBox.uncheck()
+            await expect(CheckBox).not.toBeChecked()
+        }else{
+            await CheckBox.check()
+            await expect(CheckBox).toBeChecked()
+        }
+    }
+
+    // check random boxes
+    const arr:number[]=[0,1]
+    for(const num of arr){
+        const CheckBox:Locator=page.getByLabel(labels[num])
+        await CheckBox.check()
+        await expect(CheckBox).toBeChecked()
+    }
+}) 
