789a45451cad2dfb430ce9b8db97730e90917734
PONNARASU-VK <vponnarasu07@gmail.com>
2026-08-18 17:32:22 +0530

    error solved

--- tests/sortDropdown.spec.ts
+++ tests/sortDropdown.spec.ts
@@ -4,5 +4,5 @@ test('Verify multiple dropdown', async({page})=>{
    await page.goto('https://playwright-html-actions.netlify.app/')
    const lists:string[]=(await page.locator('#sortTableSelect').allTextContents()).map(value => value.trim())
    const sorted:string[]=[...lists].sort()
-   expect(sorted).toEqual(lists)
+   expect(sorted).toEqual(lists) 
 })
