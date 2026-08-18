2ad55c16ffb34880d7c85ed144d9d76ff08c29ab
PONNARASU-VK <vponnarasu07@gmail.com>
2026-08-18 16:57:28 +0530

    multipleDropdown tests

---
+++ tests/multipleDropdown.spec.ts
@@ -0,0 +1,9 @@
+import {test, expect, Locator} from "@playwright/test"
+
+test('Verify multiple dropdown', async({page})=>{
+   await page.goto('https://playwright-html-actions.netlify.app/')
+   await page.locator('#toolsMultiSelect').selectOption(['Playwright','Cypress'])
+   await expect(page.locator('#toolsMultiSelect')).toHaveValues(['Playwright', 'Cypress'])
+   const counts:Locator=page.locator('#toolsMultiSelect option')
+   await expect(counts).toHaveCount(6)
+})
