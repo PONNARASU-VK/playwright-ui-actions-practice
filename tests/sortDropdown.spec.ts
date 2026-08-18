675b32fa301d9fb61c3a21169956ed80d8b3f2d7
PONNARASU-VK <vponnarasu07@gmail.com>
2026-08-18 17:02:17 +0530

    sortedDropdown test

---
+++ tests/sortDropdown.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect, Locator} from "@playwright/test"
+
+test('Verify multiple dropdown', async({page})=>{
+   await page.goto('https://playwright-html-actions.netlify.app/')
+   const lists:string[]=(await page.locator('#sortTableSelect').allTextContents()).map(value => value.trim())
+   const sorted:string[]=[...lists].sort()
+   expect(sorted).toEqual(lists)
+})
